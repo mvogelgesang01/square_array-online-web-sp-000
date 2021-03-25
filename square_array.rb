695e98374eacbab0e78ca42de53b66f29efc0592
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-25 20:48:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  new_numbers = [1,2,3]
-  numbers.each do |num|
-    new_numbers << (num ** 2)
-  end
-  return new_numbers
+  new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
 end
